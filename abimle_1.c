96ebcaf4a08eb89a18df6d7cfe217420070ff92b
omerf-c <kralfarukcetin@gmail.com>
2026-03-13 20:58:53 +0300

    yeni bilgi denemesi

--- abimle_1.c
+++ abimle_1.c
@@ -1,3 +1,36 @@
-//selam bu deneme
-// Created by ömer on 9.03.2026.
-//
+#include <stdio.h>
+#include <string.h>
+#define N 10
+#include <ctype.h>
+
+
+int main() {
+
+    int i=0,count=0;
+    char trupin[N];
+   const char const pin[N] = "3647549";
+    char *p;
+p=&pin;
+
+printf("hint:");
+   while(*p!='\0') {
+    if (isdigit(p) == 0) {
+        count++;
+    }
+     p++;
+    }
+    printf("your password is %d digits long .\n",count);
+
+
+
+        do {
+            printf("enter your password: ");
+            fgets(trupin,7,stdin);
+
+            }while (strcmp(pin,trupin)!=0);
+    printf("welcome");
+
+
+
+return 0;
+}
