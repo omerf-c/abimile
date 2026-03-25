55f16fc1cd074d0fca75b2ecb94ca63c1ce80e0a
omerf-c <kralfarukcetin@gmail.com>
2026-03-25 20:23:07 +0300

    yeni bilgi denemesi

--- abimle_1.c
+++ abimle_1.c
@@ -1,30 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+/* strcpy, strncpy, strcat, strncat fonksiyonlari */
 #include <stdio.h>
-
+#include <string.h>
 int main()
 {
-    int i = 0, j = 0;
-    int c;
-    char cumle[80],cumle2[80];
+    char s1[9] = "Muhammet";
+    char s2[9] = "Yorulmaz";
+    char s3[9];
+    char s4[9];
+    char s5[80];
 
-    puts("Bir cumle giriniz: ");
+    strcpy(s3, s1);
+    strncpy(s4, s2, 7);
+    s4[7] = '\0';   /* son karaktere \0 degeri ataniyor */
 
-    // Nokta'ya kadar oku
-    while( (c = getchar()) != '.' && c != '\n' && c != EOF )
-        cumle[i++] = c;
-
-    cumle[i] = '\0';
-
-    // Nokta'dan sonra oku
-    if (c == '.') {
-        while( (c = getchar()) != '\n' && c != EOF )
-            cumle2[j++] = c;
-    }
-    cumle2[j] = '\0';
-
-    puts("Girilen cumle oncesi:");
-    puts(cumle);
-    puts("Girilen cumle sonrasi:");
-    puts(cumle2);
-
+    printf("s1 = %s \n", s1);
+    printf("s2 = %s \n", s2);
+    printf("s3 = %s \n", s3);
+    printf("s4 = %s \n", s4);
+    strcpy(s5, s4);
+    strcat(s5, s1);
+    strncat(s5, s2, 7);
+    printf(" s5 = %s \n", s5);
     return 0;
 }
