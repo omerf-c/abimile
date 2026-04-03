52e45af4a0fb93f5f167b2a3c7d871c336c42973
omerf-c <kralfarukcetin@gmail.com>
2026-04-03 13:34:47 +0300

    yeni bilgi denemesi

--- abimle_1.c
+++ abimle_1.c
@@ -1,26 +1,22 @@
-
-
 #include <stdio.h>
-#include <string.h>
-int main()
-{
-    char s1[9] = "Muhammet";
-    char s2[9] = "Yorulmaz";
-    char s3[9];
-    char s4[9];
-    char s5[80];
 
-    strcpy(s3, s1);
-    strncpy(s4, s2, 7);
-    s4[7] = '\0';
+int main() {
+    FILE *dosya;
+    char name[50];
+    int not;
+    dosya = fopen("notlar.txt", "w+");
+    if(dosya == NULL) {
+        printf("Dosya acilamadi");
+        return 1;
+    }
+    printf("Adinizi giriniz: ");
+    fgets(name,50,stdin);
+    printf("Notunuzu giriniz: ");
+    scanf("%d", &not);
+    fprintf(dosya, "%s: %d\n", name, not);
+    fclose(dosya);
 
-    printf("s1 = %s \n", s1);
-    printf("s2 = %s \n", s2);
-    printf("s3 = %s \n", s3);
-    printf("s4 = %s \n", s4);
-    strcpy(s5, s4);
-    strcat(s5, s1);
-    strncat(s5, s2, 7);
-    printf(" s5 = %s \n", s5);
+    printf("Dosyaya yazildi!");
+
     return 0;
 }
